2c5a8541cca63aa3db7a584f2ffb9e322283db5e
Hritik408 <chaudharyhritik91@gmail.com>
2024-07-10 01:42:06 +0530

    ..

--- src/components/Trash_area.jsx
+++ src/components/Trash_area.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { clearTrash, removeItem } from "../utils/trashSlice";
 import Bottom from "./Bottom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HiEye } from "react-icons/hi";
 import { IoDocumentTextOutline } from "react-icons/io5";
 
@@ -21,31 +21,39 @@ const Trash_area = () => {
     settextLarge(!textLarge);
     setshowIcon(!showIcon);
   };
+  
 
   return (
     <>
-      <div className="bg-stone-200 w-[62%] flex flex-col h-screen">
-        <div className="flex flex-col flex-grow">
-          {items.map((item) => (
-            <div
-              key={item.id}
-              className="flex justify-between p-1 m-1 border-b-2 border-neutral-300"
-            >
-              <div className={`text ${textLarge ? "text-2xl" : " "}`}>
-                {item.text}
-              </div>
-              <button
-                className="text-red-500 ml-3"
-                onClick={() => handleRemoveItem(item.id)}
-              >
-                Remove
-              </button>
-            </div>
-          ))}
-        </div>
-        <div className="mt-auto p-2 bg-gray-200">
-          {/* <Bottom /> */}
-          <div className="bg-gray-800 mx-[-8px] mb-[-8px] ">
+    
+    <div className="bg-stone-200 w-[62%] flex flex-col h-screen">
+
+<div className="flex flex-col flex-grow">
+  {items.map((item) => (
+    <div
+      key={item.id}
+      className="flex justify-between p-1 m-1 border-b-2 border-neutral-300"
+    >
+      <div className={`text ${textLarge ? "text-2xl" : " "}`}>
+        {item.text}
+      </div>
+      <button
+        className="text-red-500 ml-3"
+        onClick={() => handleRemoveItem(item.id)}
+      >
+        Remove
+      </button>
+    </div>
+  ))}
+</div>
+
+    
+     
+
+        <div className="mt-auto p-2 bg-gray-200 ">
+      
+          <div className="bg-gray-800 mx-[-8px] mb-[-8px] flex">
+
             <button onClick={toggleTextSize}>
               {showIcon ? (
                 <IoDocumentTextOutline className="ml-2 mt-1 text-white text-xl" />
@@ -53,6 +61,8 @@ const Trash_area = () => {
                 <HiEye className="ml-2 mt-1 text-white text-xl" />
               )}
             </button>
+        
+
           </div>
         </div>
       </div>
@@ -60,4 +70,4 @@ const Trash_area = () => {
   );
 };
 
-export default Trash_area;
+export default Trash_area; 
