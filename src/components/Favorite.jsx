a639a31b110b50a7ae7c2a402e2f02ea39e3898b
Hritik408 <chaudharyhritik91@gmail.com>
2024-09-05 01:49:39 +0530

    ..

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,10 +1,12 @@
 import { useSelector } from "react-redux";
 import Search from "./Search";
-
-function Favorite() {
+// import { useEffec } from "react";
+// function Favorite({ notes, onNoteClick }) {
+function Favorite({names}) {
+   
 
     const item = useSelector((store) => store.favorite.items);
-    // const prod = useSelector((store) => store.temp.items);
+
     
     return (
         <>
@@ -12,17 +14,57 @@ function Favorite() {
         <div className="flex flex-col flex-grow bg-red-50">
             {item.map((it) => (
                 <div className="pl-2 pt-2">{it}</div>
-            ))}
-
-            {/* {prod.map((it) => (
-                <div className="pl-2 pt-2 text-orange-400">{it}</div>
-            ))} */}
-            
-        </div> 
-         {/* {items} */}
+                
+            ))} 
+      
+            </div>
       
          </>
     )
 }
 
 export default Favorite;
+
+
+// import React from 'react';
+// import Search from './Search';
+// import { useSelector } from "react-redux";
+
+
+// function Favorite({ notes, onNoteClick }) {
+//   const savedNames = Object.keys(notes);
+
+//  const item = useSelector((store) => store.favorite.items);
+
+
+//   return (
+//     <>
+//     <Search />
+
+//     <div className="flex flex-col flex-grow bg-red-50">
+//              {item.map((it) => (
+//                  <div className="pl-2 pt-2">{it}</div>                
+//              ))}   
+//            </div>
+
+//         <div className="mt-4">
+//         <h3 className="text-white">Favorite Notes</h3>
+//         <ul>
+//             {savedNames.map((name) => (
+//             <li
+//                 key={name}
+//                 onClick={() => onNoteClick(name)}
+//                 className="cursor-pointer text-blue-300"
+//             >
+//                 {name}
+//             </li>
+//             ))}
+//         </ul>
+//         </div>
+//     </>
+//   );
+// }
+
+// export default Favorite;
+
+
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,82 +1,89 @@
 import React, { useContext, useState } from "react";
 import { useDispatch } from "react-redux";
-import { addItem } from "../utils/trashSlice";
 import UserContext from "../utils/UserContext";
+import { addItem } from "../utils/trashSlice";
+import { addThing } from "../utils/FavoriteSlice";
 import { addSaved } from "../utils/savedSlice";
 import { addtemp } from "../utils/tempSlice";
-import { addThing } from "../utils/FavoriteSlice";
+import Favorite from "./Favorite";
 
-function Text({myvalue}) {
-
-  const [inputText, setinputText] = useState(myvalue);
-  const [isSaved, setisSaved] = useState(false);
+function Text({ myvalue }) {
+  const [inputText, setInputText] = useState(myvalue);
+  const [isSaved, setIsSaved] = useState(false);
+  const [savedNotes, setSavedNotes] = useState({});
   const { loggedInUser, setuserName } = useContext(UserContext);
 
   const dispatch = useDispatch();
-  // const items = useSelector(state => state.trash.items);
-
   const handleSave_trash = () => {
     dispatch(addItem(inputText));
-    setinputText(""); // clear the text after saving
+    setInputText(""); // clear the text after saving
   };
 
   const handleSave_favorite = () => {
     dispatch(addThing(inputText));
-    setinputText(""); // clear the text after saving
+    setInputText(""); // clear the text after saving
   };
 
   const handleChange = (e) => {
     const value = e.target.value;
-    setinputText(value);
-    // console.log(value);
+    setInputText(value);
 
     if (!isSaved && value.length <= 45) {
       setuserName(value);
-      //  dispatch(addSaved(loggedInUser));
     }
   };
 
   const handleKeyDown = (e) => {
-    if (e.key == "Enter" && !isSaved) {
-      setisSaved(true);
-    //  console.log(e.target.value);
-    const value = e.target.value;
-      // dispatch(addSaved(e.target.value));
+    if (e.key === "Enter" && !isSaved) {
+      setIsSaved(true);
+      const value = e.target.value;
+      setSavedNotes({
+        ...savedNotes,
+        [loggedInUser]: value,
+      });
       dispatch(addtemp(value));
-    } 
-    else if (
-      e.key == "Backspace" &&
+    } else if (
+      e.key === "Backspace" &&
       isSaved &&
-      e.target.selectionStart == loggedInUser.length
+      e.target.selectionStart === loggedInUser.length
     ) {
-      setisSaved(false);
+      setIsSaved(false);
       setuserName(inputText);
       dispatch(addSaved(inputText));
     }
   };
 
-  // const{loggedInUser, setuserName} = useContext(UserContext);
+  const openNote = (name) => {
+    setuserName(name);
+    setInputText(savedNotes[name]);
+    setIsSaved(true); // Prevent further saving as this is already a saved note
+  };
 
   return (
-    <>
-      <div className="bg-slate-600 w-[77%]">
+    <div className="bg-slate-600 w-[77%] p-4">
+      <div className="mb-4">
         <button className="text-blue-500" onClick={handleSave_trash}>
           Trash
         </button>
         <button className="text-purple-500 ml-3" onClick={handleSave_favorite}>
           Favorite
         </button>
-
-        <textarea
-          className="bg-slate-400 h-screen w-full pl-3"
-          value={inputText}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-        ></textarea>
-
       </div>
-    </>
+
+      <textarea
+        className="bg-slate-400 h-screen w-full p-3"
+        value={inputText}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      ></textarea>
+
+      {/* <Favorite notes={savedNotes} onNoteClick={openNote} /> */}
+    </div>
   );
 }
 
 export default Text;
+
+
+
+
